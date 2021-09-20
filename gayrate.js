4d55feeb016be398bc2cd7241211d7c4b23b3739
JoeSchmoe69 <86168518+JoeSchmoe69@users.noreply.github.com>
2021-09-20 17:32:38 +0400

    Add files via upload

---
+++ gayrate.js
@@ -0,0 +1,7 @@
+client.on('message', message => {
+  console.log(`[${message.author.tag}]: ${message.content}`);
+   if (message.content === `brh!gayrate `) {
+     var rating = Math.floor(Math.random() * 100) + 1;
+     message.reply(`You are ${rating}% gay!🥶😳`);
+   }
+  });
